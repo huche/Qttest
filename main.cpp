8d42cb585899699d48aa4e3118dc8cb1ed1d9589
TestGitCX <2877067795@qq.com>
2016-04-16 14:54:29 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle("QT窗口测试标题");
+    w.setMaximumSize(640,480);//设置最大窗口
+//    w.setMinimumSize(10,80);
+    w.move(100,100);//设置起始位置
+//    w.setStyleSheet("background:blue");//设置背景颜色
+    w.setWindowIcon(QIcon(":/icon/battery_2.ico"));
+    w.show();
+
+    return a.exec();
+}
